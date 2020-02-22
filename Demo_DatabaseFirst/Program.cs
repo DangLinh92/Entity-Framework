9d2895f0caefbfb266fb65c709b7bc29d2502c42
DangLinh92 <hoamoclan.3192@gmail.com>
2020-02-22 10:02:17 +0700

    Demo database first

---
+++ Demo_DatabaseFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new CinemaDBEntities();
+            var Chude = new ChuDe()
+            {
+                Id = 7,
+                TenChuDe = "Tâm lý"
+            };
+            context.ChuDes.Add(Chude);
+            context.SaveChanges();
+        }
+    }
+}
